2dc6794ce7cc931285d0b9e3279e27661d168af9
Abhishek Anand <anand2000ys@gmail.com>
2023-04-09 15:14:17 +0530

    files removed and navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Canva</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Design spotlight" id="basic-nav-dropdown">
+              <NavDropdown.Item>Visual Documents</NavDropdown.Item>
+              <NavDropdown.Item>Photos and videos</NavDropdown.Item>
+              <NavDropdown.Item>Print</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Business" id="basic-nav-dropdown">
+              <NavDropdown.Item>Visual Documents</NavDropdown.Item>
+              <NavDropdown.Item>Photos and videos</NavDropdown.Item>
+              <NavDropdown.Item>Print</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Education" id="basic-nav-dropdown">
+              <NavDropdown.Item>Visual Documents</NavDropdown.Item>
+              <NavDropdown.Item>Photos and videos</NavDropdown.Item>
+              <NavDropdown.Item>Print</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Plans and pricing" id="basic-nav-dropdown">
+              <NavDropdown.Item>Visual Documents</NavDropdown.Item>
+              <NavDropdown.Item>Photos and videos</NavDropdown.Item>
+              <NavDropdown.Item>Print</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Learn" id="basic-nav-dropdown">
+              <NavDropdown.Item>Visual Documents</NavDropdown.Item>
+              <NavDropdown.Item>Photos and videos</NavDropdown.Item>
+              <NavDropdown.Item>Print</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
