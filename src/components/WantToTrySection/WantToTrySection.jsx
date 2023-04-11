78e0b7b19aa76d49611e2c3c9bdd8cf61e3228d4
Abhishek Anand <anand2000ys@gmail.com>
2023-04-11 17:54:38 +0530

    card images updated

--- src/components/WantToTrySection/WantToTrySection.jsx
+++ src/components/WantToTrySection/WantToTrySection.jsx
@@ -3,6 +3,17 @@ import "./WantToTrySection.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import docs from "../../assets/docs.jpg";
+import facebookcover from "../../assets/facebook cover.png";
+import facebookpost from "../../assets/facebook post.jpg";
+import instagrampost from "../../assets/instagram post.jpg";
+import instagramstory from "../../assets/instagram story.jpg";
+import invitation from "../../assets/invitation.jpg";
+import logo from "../../assets/logo.png";
+import phonewallpaper from "../../assets/phone wallpaper.jpg";
+import poster from "../../assets/poster.jpg";
+import whiteboard from "../../assets/whiteboard.png";
+import yourstory from "../../assets/your story.jpg";
+import youtube from "../../assets/youtube.jpg";
 
 function TrySection() {
   const responsive = {
@@ -13,7 +24,7 @@ function TrySection() {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 7,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -33,20 +44,48 @@ function TrySection() {
           <h2>Doc</h2>
         </div>
         <div className="card">
-          <img className="product-image" src={docs} alt="doc-img" />
-          <h2>Doc</h2>
+          <img className="product-image" src={facebookcover} alt="doc-img" />
+          <h2>Facebook Cover</h2>
         </div>
         <div className="card">
-          <img className="product-image" src={docs} alt="doc-img" />
-          <h2>Doc</h2>
+          <img className="product-image" src={facebookpost} alt="doc-img" />
+          <h2>Facebook Post</h2>
         </div>
         <div className="card">
-          <img className="product-image" src={docs} alt="doc-img" />
-          <h2>Doc</h2>
+          <img className="product-image" src={instagrampost} alt="doc-img" />
+          <h2>Instagram Post</h2>
         </div>
         <div className="card">
-          <img className="product-image" src={docs} alt="doc-img" />
-          <h2>Doc</h2>
+          <img className="product-image" src={instagramstory} alt="doc-img" />
+          <h2>Instagram Story</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={invitation} alt="doc-img" />
+          <h2>Invitation</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={logo} alt="doc-img" />
+          <h2>Logo</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={phonewallpaper} alt="doc-img" />
+          <h2>Phone Wallpaper</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={poster} alt="doc-img" />
+          <h2>Poster</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={whiteboard} alt="doc-img" />
+          <h2>Whiteboard</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={yourstory} alt="doc-img" />
+          <h2>Your Story</h2>
+        </div>
+        <div className="card">
+          <img className="product-image" src={youtube} alt="doc-img" />
+          <h2>Youtube Thumbnail</h2>
         </div>
       </Carousel>
     </div>
